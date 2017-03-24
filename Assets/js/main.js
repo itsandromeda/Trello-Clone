7a1be933c2e5c71b9a60eaa17fe76732455b4eb1
itsandromeda <itsandromeda@gmail.com>
2017-03-24 14:26:18 -0500

    Add a list - List box css

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -4,21 +4,11 @@ var container = document.getElementById("container"),
     addListBtn = document.getElementById("add-a-list-btn"),
     saveListBtn = document.getElementById("save-list-btn"),
     saveListBox = document.getElementById("save-list-box"),
-    //addACardBtn = document.createElement("div"),
     listInputBox = document.getElementById("list-input-box"),
-    
-    //cardsList = document.createElement("div"),  
-    //addCardBtn = document.createElement("button"),
-    //newCardNode = document.createTextNode("Add a card..."),
-    //newCard = document.createElement("a"),
-    //cardContainer = document.createElement("textarea"),
-    //newAddCardBtn = document.createTextNode("Add"),
-    //newCardTitleNode = document.createTextNode(cardContainer.value),
     textValue;
-    //form = document.getElementById("form");
 
 function addList() {
-    "use strict";    
+    "use strict";
     saveListBox.style.display = "inline-block";
     addListBtn.style.display = "none";
 }
@@ -28,24 +18,30 @@ function createList(textValue) {
     if (textValue) {
         var parentDiv = document.createElement("div"),
             listTitle = document.createElement("div"),
+            title = document.createElement("div"),
+            dotIcon = document.createElement("a"),
             newTextNode = document.createTextNode(textValue),
             addCardLink = document.createElement("a"),
             addText = document.createTextNode("Add a card...");
         
-        //<a>Add a card...</a>
-        //<div class="fa fa-ellipsis-h" style="font-size:12px;color:white"></div> ... icono
+        parentDiv.setAttribute("class", "add-card-container");
+        dotIcon.setAttribute("class", "fa fa-ellipsis-h ellipsis");
+        title.setAttribute("contenteditable", "true");
+        title.setAttribute("class", "text");
+        title.appendChild(newTextNode);
+        listTitle.appendChild(dotIcon);
+        listTitle.setAttribute("class", "list-title");
+        listTitle.appendChild(title);
         addCardLink.setAttribute("href", "#");
+        addCardLink.setAttribute("class", "add-card");
         addCardLink.appendChild(addText);
-        parentDiv.setAttribute("class", "add-a-card-btn");
-        listTitle.setAttribute("class", "text");
-        listTitle.appendChild(newTextNode);
         parentDiv.style.cssFloat = "left";
         parentDiv.style.display = "inline-block";
-
-        container.insertBefore(parentDiv, addListBtn); 
-        saveListBox.style.display = "inline-block";
         parentDiv.appendChild(listTitle);
         parentDiv.appendChild(addCardLink);
+        container.insertBefore(parentDiv, addListBtn);
+        addListBtn.style.display = "inline-block";
+        saveListBox.style.display = "none";
         listInputBox.value = "";
     }
 }
@@ -60,4 +56,5 @@ window.addEventListener("load", function () {
         textValue = listInputBox.value;
         createList(textValue);
     });
+    
 });
